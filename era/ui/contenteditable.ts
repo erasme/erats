d3f220a3db58c62eaffee66bc134bfd3df347e7c
Daniel LACROIX <dlacroix@erasme.org>
2018-06-07 14:37:59 +0200

    add event to monitor the text selection of ContentEditable

--- era/ui/contenteditable.ts
+++ era/ui/contenteditable.ts
@@ -8,19 +8,35 @@ namespace Ui {
     export class ContentEditable extends Html {
         anchorNode?: Node;
         anchorOffset: number = 0;
+        private _hasSelection: boolean = false;
         readonly anchorchanged = new Core.Events<{ target: ContentEditable }>();
         set onanchorchanged(value: (event: { target: ContentEditable }) => void) { this.anchorchanged.connect(value); }
         readonly changed = new Core.Events<{ target: ContentEditable }>();
         set onchanged(value: (event: { target: ContentEditable }) => void) { this.changed.connect(value); }
         readonly validated = new Core.Events<{ target: ContentEditable }>();
         set onvalidated(value: (event: { target: ContentEditable }) => void) { this.validated.connect(value); }
+        readonly selectionentered = new Core.Events<{ target: ContentEditable }>();
+        set onselectionentered(value: (event: { target: ContentEditable }) => void) { this.selectionentered.connect(value); }
+        readonly selectionleaved = new Core.Events<{ target: ContentEditable }>();
+        set onselectionleaved(value: (event: { target: ContentEditable }) => void) { this.selectionleaved.connect(value); }
 
         private _lastHtml = '';
     
         constructor(init?: ContentEditableInit) {
             super(init);
             this.selectable = true;
+            this.focusable = true;
+            (this.drawing as HTMLDivElement).removeAttribute('tabindex');
             this.htmlDrawing.setAttribute('contenteditable', 'true');
+            this.drawing.addEventListener('blur', (e) => {
+                let node = window.getSelection().anchorNode;
+                if (window.getSelection().isCollapsed && this._hasSelection) {
+                    this._hasSelection = false;
+                    this.selectionleaved.fire({ target: this });
+                }
+                this.onBlur();
+            }, true);
+            this.drawing.addEventListener('focus', (e) => this.onFocus(), true);
             this.drawing.addEventListener('keyup', (e) => this.onKeyUp(e));
             this.drawing.addEventListener('DOMSubtreeModified', (e) => this.onContentSubtreeModified(e));
             if (init) {
@@ -30,7 +46,17 @@ namespace Ui {
                     this.changed.connect(init.onchanged);
                 if (init.onvalidated)
                     this.validated.connect(init.onvalidated);
-            }	
+            }
+        }
+
+        protected onLoad() {
+            super.onLoad();
+            document.addEventListener('selectionchange', this.testAnchorChange);
+        }
+
+        protected onUnload() {
+            super.onUnload();
+            document.removeEventListener('selectionchange', this.testAnchorChange);
         }
 
         protected onKeyUp(event) {
@@ -41,8 +67,29 @@ namespace Ui {
                 this.validated.fire({ target: this });
         }
 
+        protected testAnchorChange = () => {
+            let node = window.getSelection().anchorNode;
+            // check if it's a child node
+            let currentNode = node;
+            while (currentNode != null && currentNode != this.drawing) {
+                currentNode = currentNode.parentNode;
+            }
+            let hasSelection = (currentNode != null);
+            if (this._hasSelection != hasSelection) {
+                this._hasSelection = hasSelection;
+                if (hasSelection)
+                    this.selectionentered.fire({ target: this });
+                else
+                    this.selectionleaved.fire({ target: this });
+            }
+            if (!hasSelection) {
+                if (this.hasFocus)
+                    this.onBlur();
+                return;
+            }
+            if (!this.hasFocus)
+                this.onFocus();
 
-        protected testAnchorChange() {
             if ((window.getSelection().anchorNode != this.anchorNode) ||
                 (window.getSelection().anchorOffset != this.anchorOffset)) {
                 this.anchorNode = window.getSelection().anchorNode;
@@ -66,4 +113,3 @@ namespace Ui {
         }
     }
 }	
-    
