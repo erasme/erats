9834804dbd0b445e7fd2f6ab283ebc7cf3a19370
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2018-07-09 09:17:23 +0200

    additional formattings to richtexteditor

--- era/era.js
+++ era/era.js
@@ -22928,7 +22928,6 @@ var Ui;
             return _this;
         }
         ComboPopup.prototype.onSearchChange = function (field, value) {
-            console.log("onSearchChange value: " + value + ", allowNone: " + this.allowNone);
             if (value == '' && this.allowNone)
                 this.emptyField.show();
             else
@@ -27431,6 +27430,11 @@ var Ui;
                 ontoggled: function () { return document.execCommand('italic', false, null); },
                 onuntoggled: function () { return document.execCommand('italic', false, null); }
             });
+            var underlineButton = new RichTextButton().assign({
+                icon: 'format-underline', focusable: false,
+                ontoggled: function () { return document.execCommand('underline', false, null); },
+                onuntoggled: function () { return document.execCommand('underline', false, null); }
+            });
             var alignLeftButton = new RichTextButton().assign({
                 icon: 'format-align-left', focusable: false,
                 ontoggled: function () { return document.execCommand('justifyLeft', false, null); },
@@ -27446,14 +27450,69 @@ var Ui;
                 ontoggled: function () { return document.execCommand('justifyRight', false, null); },
                 onuntoggled: function () { return document.execCommand('justifyRight', false, null); }
             });
+            var insertOrderedListButton = new RichTextButton().assign({
+                icon: 'format-insert-ordered-list', focusable: false,
+                ontoggled: function () { return document.execCommand('insertOrderedList', false, null); },
+                onuntoggled: function () { return document.execCommand('insertOrderedList', false, null); }
+            });
+            var insertUnorderedListButton = new RichTextButton().assign({
+                icon: 'format-insert-unordered-list', focusable: false,
+                ontoggled: function () { return document.execCommand('insertUnorderedList', false, null); },
+                onuntoggled: function () { return document.execCommand('insertUnorderedList', false, null); }
+            });
+            var insertImageButton = new Ui.Button().assign({
+                icon: 'format-insert-image', focusable: false,
+                style: {
+                    borderWidth: 0,
+                    background: 'rgba(255,255,255,0)',
+                    activeBackground: 'rgba(255,255,255,0)'
+                },
+                onpressed: function () {
+                    var url = window.prompt("URL de l'image à insérer :");
+                    if (url) {
+                        document.execCommand('insertImage', false, url);
+                    }
+                }
+            });
+            var insertURLButton = new Ui.Button().assign({
+                icon: 'format-insert-url', focusable: false,
+                style: {
+                    borderWidth: 0,
+                    background: 'rgba(255,255,255,0)',
+                    activeBackground: 'rgba(255,255,255,0)'
+                },
+                onpressed: function () {
+                    var url = window.prompt("URL à insérer :");
+                    if (url) {
+                        document.execCommand('createLink', false, url);
+                    }
+                }
+            });
+            var quoteButton = new Ui.Button().assign({
+                icon: 'format-quote', focusable: false,
+                style: {
+                    borderWidth: 0,
+                    background: 'rgba(255,255,255,0)',
+                    activeBackground: 'rgba(255,255,255,0)'
+                },
+                onpressed: function () {
+                    document.execCommand('formatBlock', false, '<blockquote>');
+                }
+            });
             var controls = new Ui.HBox().assign({
                 uniform: true, isDisabled: false,
                 content: [
                     boldButton,
                     italicButton,
+                    underlineButton,
+                    quoteButton,
                     alignLeftButton,
                     alignCenterButton,
-                    alignRightButton
+                    alignRightButton,
+                    insertOrderedListButton,
+                    insertUnorderedListButton,
+                    insertImageButton,
+                    insertURLButton
                 ]
             });
             var bg = new Ui.TextBgGraphic();
@@ -27465,6 +27524,7 @@ var Ui;
                 onanchorchanged: function () {
                     boldButton.isActive = document.queryCommandState('bold');
                     italicButton.isActive = document.queryCommandState('italic');
+                    underlineButton.isActive = document.queryCommandState('underline');
                     alignLeftButton.isActive = document.queryCommandState('justifyLeft');
                     alignCenterButton.isActive = document.queryCommandState('justifyCenter');
                     alignRightButton.isActive = document.queryCommandState('justifyRight');
@@ -27596,7 +27656,13 @@ var Ui;
 })(Ui || (Ui = {}));
 Ui.Icon.register('format-bold', "M31.2 21.58c1.93-1.35 3.3-3.53 3.3-5.58 0-4.51-3.49-8-8-8h-12.5v28h14.08c4.19 0 7.42-3.4 7.42-7.58 0-3.04-1.73-5.63-4.3-6.84zm-11.2-8.58h6c1.66 0 3 1.34 3 3s-1.34 3-3 3h-6v-6zm7 18h-7v-6h7c1.66 0 3 1.34 3 3s-1.34 3-3 3z");
 Ui.Icon.register('format-italic', "M20 8v6h4.43l-6.86 16h-5.57v6h16v-6h-4.43l6.86-16h5.57v-6z");
+Ui.Icon.register('format-underline', "M24 34c6.63 0 12-5.37 12-12v-16h-5v16c0 3.87-3.13 7-7 7s-7-3.13-7-7v-16h-5v16c0 6.63 5.37 12 12 12zm-14 4v4h28v-4h-28z");
 Ui.Icon.register('format-align-left', "M30 30h-24v4h24v-4zm0-16h-24v4h24v-4zm-24 12h36v-4h-36v4zm0 16h36v-4h-36v4zm0-36v4h36v-4h-36z");
 Ui.Icon.register('format-align-right', "M6 42h36v-4h-36v4zm12-8h24v-4h-24v4zm-12-8h36v-4h-36v4zm12-8h24v-4h-24v4zm-12-12v4h36v-4h-36z");
 Ui.Icon.register('format-align-center', "M14 30v4h20v-4h-20zm-8 12h36v-4h-36v4zm0-16h36v-4h-36v4zm8-12v4h20v-4h-20zm-8-8v4h36v-4h-36z");
+Ui.Icon.register('format-insert-ordered-list', "M4 34h4v1h-2v2h2v1h-4v2h6v-8h-6v2zm2-18h2v-8h-4v2h2v6zm-2 6h3.6l-3.6 4.2v1.8h6v-2h-3.6l3.6-4.2v-1.8h-6v2zm10-12v4h28v-4h-28zm0 28h28v-4h-28v4zm0-12h28v-4h-28v4z");
+Ui.Icon.register('format-insert-unordered-list', "M8 21c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3zm0-12c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3zm0 24.33c-1.47 0-2.67 1.19-2.67 2.67s1.2 2.67 2.67 2.67 2.67-1.19 2.67-2.67-1.2-2.67-2.67-2.67zm6 4.67h28v-4h-28v4zm0-12h28v-4h-28v4zm0-16v4h28v-4h-28z");
+Ui.Icon.register('format-insert-image', "M42 38v-28c0-2.21-1.79-4-4-4h-28c-2.21 0-4 1.79-4 4v28c0 2.21 1.79 4 4 4h28c2.21 0 4-1.79 4-4zm-25-11l5 6.01 7-9.01 9 12h-28l7-9z");
+Ui.Icon.register('format-insert-url', "M7.8 24c0-3.42 2.78-6.2 6.2-6.2h8v-3.8h-8c-5.52 0-10 4.48-10 10s4.48 10 10 10h8v-3.8h-8c-3.42 0-6.2-2.78-6.2-6.2zm8.2 2h16v-4h-16v4zm18-12h-8v3.8h8c3.42 0 6.2 2.78 6.2 6.2s-2.78 6.2-6.2 6.2h-8v3.8h8c5.52 0 10-4.48 10-10s-4.48-10-10-10z");
+Ui.Icon.register('format-quote', "M12 34h6l4-8v-12h-12v12h6zm16 0h6l4-8v-12h-12v12h6z");
 //# sourceMappingURL=era.js.map
