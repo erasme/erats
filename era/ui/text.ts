981fc004addbdef431a76b0e6cdf3766b858628e
daniel <dlacroix@erasme.org>
2019-11-25 13:45:13 +0100

    cleanup

--- era/ui/text.ts
+++ era/ui/text.ts
@@ -1,23 +1,12 @@
 namespace Ui
 {
     export interface TextInit extends CompactLabelInit {
-//        textTransform?: string;
     }
 
     export class Text extends CompactLabel implements TextInit
     {
         constructor(init?: TextInit) {
             super(init);
-//            this.drawing.style.whiteSpace = 'pre-wrap';
-//            if (init) {
-//                if (init.textTransform !== undefined)
-//                    this.textTransform = init.textTransform;	
-//            }
         }
-
-//        set textTransform(textTransform: string) {
-//            this.drawing.style.textTransform = textTransform;
-//            this.invalidateMeasure();
-//        }
     }
 }	
