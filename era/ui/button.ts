ad57cc70f4736fd19857d685e51cb4722ea43fe7
Daniel LACROIX <dlacroix@erasme.org>
2021-07-09 13:50:47 +0200

    correct Ui.FlatButton style when active

--- era/ui/button.ts
+++ era/ui/button.ts
@@ -607,7 +607,9 @@ namespace Ui {
         static style: object = {
             borderWidth: 1,
             background: 'rgba(250,250,250,0)',
-            backgroundBorder: 'rgba(140,140,140,0)'
+            backgroundBorder: 'rgba(140,140,140,0)',
+            activeBackground: 'rgba(250,250,250,0)',
+            activeBackgroundBorder: 'rgba(140,140,140,0)'
         }
     }
 }
