bf415fc4ff3c9c3db2033a7d16032a0856db6076
Nelson DINIS GONCALVES <ndinisgoncalves@grandlyon.com>
2021-10-27 11:16:36 +0200

    Update sample to test input type/mode combo

--- samples/textfield/main.js
+++ samples/textfield/main.js
@@ -14,21 +14,62 @@ class Logs extends Ui.ScrollingArea {
             text: text, color: color, horizontalAlign: 'left'
         }));
     }
+    clear() {
+        this.logs.clear();
+    }
 }
-let app = new Ui.App();
-let vbox = new Ui.VBox({ spacing: 10, margin: 5 });
-app.content = vbox;
-let hbox = new Ui.HBox({ spacing: 10 });
-vbox.append(hbox);
-let textfield = new Ui.TextField({
-    textHolder: 'Text Holder',
-    onchanged: e => logs.log(`change: ${e.value}`)
-});
-hbox.append(textfield, true);
-let getButton = new Ui.Button({
-    text: 'get text',
-    onpressed: () => logs.log(`get text: ${textfield.value}`, 'blue')
-});
-hbox.append(getButton);
-let logs = new Logs();
-vbox.append(logs, true);
+class App extends Ui.App {
+    constructor() {
+        super();
+        let vbox = new Ui.VBox({ spacing: 10, margin: 5 });
+        this.content = vbox;
+        let toolbar = new Ui.ToolBar();
+        vbox.append(toolbar);
+        let hbox = new Ui.HBox({ spacing: 10 });
+        vbox.append(hbox);
+        let textfield = new Ui.TextField({
+            textHolder: 'Text Holder',
+            onchanged: e => logs.log(`change: ${e.value}`)
+        });
+        hbox.append(textfield, true);
+        let getButton = new Ui.Button({
+            text: 'get text',
+            onpressed: () => logs.log(`get text: ${textfield.value}`, 'blue')
+        });
+        toolbar.append(getButton);
+        let typeButton = new Ui.Combo().assign({
+            title: 'Changer le type de l\'input',
+            data: [
+                { value: 'text' }, { value: 'checkbox' }, { value: 'color' }, { value: 'date' }, { value: 'datetime-local' },
+                { value: 'file' }, { value: 'hidden' }, { value: 'image' }, { value: 'month' }, { value: 'number' }, { value: 'password' },
+                { value: 'radio' }, { value: 'range' }, { value: 'reset' }, { value: 'search' }, { value: 'submit' }, { value: 'tel' },
+                { value: 'button' }, { value: 'time' }, { value: 'url' }, { value: 'week' }, { value: 'email' }
+            ], field: 'value', position: 0,
+            onchanged: (e) => {
+                textfield.type = e.value.value;
+                logs.log(`change type: ${textfield.type}`, 'blue');
+            }
+        });
+        toolbar.append(typeButton);
+        let inputModeButton = new Ui.Combo().assign({
+            title: 'Changer l\'input mode de l\'input',
+            data: [
+                { value: 'text' }, { value: 'none' }, { value: 'decimal' }, { value: 'numeric' }, { value: 'tel' },
+                { value: 'search' }, { value: 'email' }, { value: 'url' }
+            ], field: 'value', position: 0,
+            onchanged: (e) => {
+                textfield.inputMode = e.value.value;
+                logs.log(`change inputMode: ${textfield.inputMode}`, 'blue');
+            }
+        });
+        toolbar.append(inputModeButton);
+        let clearLogButton = new Ui.Button({
+            text: 'clear logs',
+            onpressed: () => logs.clear()
+        });
+        toolbar.append(clearLogButton);
+        let logs = new Logs();
+        vbox.append(logs, true);
+    }
+}
+new App();
